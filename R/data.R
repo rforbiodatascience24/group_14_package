78d8f25984cacff050bc59bebac2e536bf93fc93
JulieBrockhoff <s204519@student.dtu.dk>
2024-10-29 09:17:23 +0100

    Codons function

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Central Dogma
+#'
+#' Overview of codons and their respective amino acid
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codons"
